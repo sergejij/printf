9195d3ffec7bc26ecd54ae84b02e9507b2740b60
Ulrick Bartemius <ubartemi@at-j5.21-school.ru>
2019-05-21 14:24:30 +0300

    work: i, d, x, o, c, s

--- ubartemi_test/test.c
+++ ubartemi_test/test.c
@@ -6,25 +6,26 @@
 /*   By: ubartemi <ubartemi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/18 16:52:47 by ubartemi          #+#    #+#             */
-/*   Updated: 2019/05/19 15:26:37 by ubartemi         ###   ########.fr       */
+/*   Updated: 2019/05/21 14:22:36 by ubartemi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
 #include <stdarg.h>
 #include <unistd.h>
-
-/*#include <stdio.h>
-
-int main(void)
-{
-    printf("%0d\n%d", 5);
-    return (0);
-}*/
+#include <stdlib.h>
+#include <string.h>
+#include "./libft/libft.h"
 
 void	ft_putchar(char c)
 {
 	write(1, &c, 1);
+}
+
+void	ft_putpointer(char *str)
+{
+    while (str++)
+	    write(1, str, 1);
 }
 
 void	ft_putnbr(int n)
@@ -52,72 +53,134 @@ void	ft_putnbr(int n)
 	}
 }
 
-void	ft_putstr(char const *s)
+int		ft_lennum_base(int num, int base)
 {
-	int counter;
+	int len;
 
-	counter = 0;
-	if (s)
+	len = 0;
+	if (num < 0 && base == 10)
+		len++;
+	if (num == 0)
+		return (1);
+	while (num > 0)
 	{
-		while (s[counter] != '\0')
-		{
-			ft_putchar(s[counter]);
-			counter++;
-		}
+		num /= base;
+		len++;
 	}
+	return (len);
 }
 
-void	ft_putnbr_base(int n, int base)
+char		*ft_itoa_base(int n, int base)
 {
+	int				len;
+	int				counter;
+	unsigned int	number;
+	char			*result;
     char *a = "0123456789abcdef";
-	int size;
-	int tmp_n;
 
-	tmp_n = n;
-	size = 1;
-	if (n < 0)
-		ft_putchar('-');
-	while (tmp_n / base != 0)
+	len = ft_lennum_base(n, base);
+	counter = 0;
+	result = (char*)malloc(sizeof(char) * (len + 1));
+	if (!result)
+		return (0);
+	result[len] = '\0';
+	if (n < 0 && base == 10)
 	{
-		size *= base;
-		tmp_n /= base;
+		result[counter] = '-';
+		number = n * (-1);
+		counter++;
 	}
-	while (size > 0)
+	else
+		number = n;
+	if (number == 0)
+		result[0] = '0';
+	while (number > 0)
 	{
-		if (n < 0)
-			ft_putchar(a[(n / size * -1)]);
-		else
-			ft_putchar(a[(n / size)]);
-		n %= size;
-		size /= base;
+		result[len - 1] = a[number % base];
+		number /= base;
+		len--;
 	}
+	return (result);
 }
 
+char *do_if_d(int d, char *str)
+{
+    int len;
+    char *s_num;
+    int size;
+    int i = 0;
 
-int ft_printf(char *apFormat, ...)
+    size =  ft_discharge(d);
+    len = ft_lennum(d);
+    s_num = (char*)malloc(sizeof(char) * (len + 1));
+    while (size > 0)
+    {
+        s_num[i] = (d / size) + 48;
+        d %= size;
+        size /= 10;
+        i++;
+    }
+    s_num[i] = '\0';
+    str = ft_strjoin(str, s_num);
+    return (str);
+}
+
+char *do_if_X(int x, char *str, int base)
+{
+    char *s_num;
+
+    s_num = ft_itoa_base(x, 16);
+    str = ft_strjoin(str, s_num);
+    return (str);
+}
+
+char *do_if_O(int o, char *str, int base)
+{
+    char *s_num;
+
+    s_num = ft_itoa_base(o, 8);
+    str = ft_strjoin(str, s_num);
+    return (str);
+}
+
+int *ft_printf(char *apFormat, ...)
 {
     va_list ap;         //указатель va_list | poit on next unnamed argument
     va_start(ap, apFormat);    // устанавливаем указатель
     char *p = apFormat;
-    
+    char *result;
+
+    result = (char*)malloc(sizeof(char));    
     int ival = 0;
     double dval = 0;
     char *sval;
     char cval;
+    char *pval;
     while (*(++p))
     {
         if (*p == '%')
             continue;
+        if (*p == 'p')
+        {
+            pval = va_arg(ap, char*);
+            continue;
+        }
         if (*p == 'd')
         {
             ival = va_arg(ap, int);
-            ft_putnbr(ival);
+            result = do_if_d(ival, result);
             continue;
         }
         else if (*p == 'x')
         {
             ival = va_arg(ap, int);
-            ft_putnbr_base(ival, 16);
+            result = do_if_X(ival, result, 16);
+            continue;
+        }
+        else if (*p == 'o')
+        {
+            ival = va_arg(ap, int);
+            result = do_if_O(ival, result, 8);
             continue;
         }
         else  if (*p == 'f')
@@ -126,28 +189,38 @@ int ft_printf(char *apFormat, ...)
             printf("%f", dval);
             continue;
         }
+        else  if (*p == 'c')
+        {
+            cval = va_arg(ap, int); // int и он будет переводить его по ascii
+            ft_strncat(result, &cval, 1);
+            continue;
+        }
         else  if (*p == 's')
         {
             sval = va_arg(ap, char*);
-            ft_putstr(sval);
+            ft_strncat(result, sval, ft_strlen(sval));
             continue;
         }
         else
         {
-            ft_putchar(*p);
+            ft_strncat(result, p, 1);
             continue;
         }
     }
+    ft_putstr(result);
     va_end(ap);
     return (0);
 }
- 
+
 int main(void)
 {
     int a = 4;
     double b = 4.222;
     char *str = "abcdefg";
     unsigned int d1 = 123;
-    ft_printf("%x %d", d1, a);
+    char sym = 'G';
+    ft_printf("%d - %d - %o - %c - %s", a, a, a + 1245, sym, str);
+    //printf("%s", y);
+    //printf("\n%p", &a);
     return 0;
 }
