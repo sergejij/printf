0fca6b5c77afc4ce5ba60917c1112322fd2d4b21
Prismari <m.ugorec@gmeil.com>
2019-07-02 16:55:16 +0300

    исправлены функции счета

--- float_without_rounding.c
+++ float_without_rounding.c
@@ -10,6 +10,7 @@ typedef struct s_len{
     size_t lenOfCurrentNbr;
     size_t lenOfResult;
     size_t lenOfPower;
+    size_t lenOfIntPart;
 } t_len;
 
 
@@ -102,24 +103,27 @@ void ft_plus_int(char *curretNum, char *cpyPower, t_len *Len)
     ft_strrev(cpyPower);
     while(*tmp_curr || *cpyPower)
     {
-       if(*tmp_curr && *cpyPower)
-           res[Len->lenOfCurrentNbr] = (((*cpyPower++ - '0') + (*tmp_curr++ - '0')) + flag);
-       else if(!(*tmp_curr) && *cpyPower)
-           res[Len->lenOfCurrentNbr] = *cpyPower++ - '0' + flag;
-       else
-           res[Len->lenOfCurrentNbr] = *tmp_curr++ -'0' + flag;
-       flag = 0;
-       if(res[Len->lenOfCurrentNbr] > 9)
-       {
-           flag = res[Len->lenOfCurrentNbr] / 10;
-           res[Len->lenOfCurrentNbr] %= 10;
-       }
-       Len->lenOfCurrentNbr++;
-   }
-   if (flag)
-       res[Len->lenOfCurrentNbr++] = flag;
-   Len->lenOfResult = 0;
-   ft_strclr(curretNum);
+        if(*tmp_curr && *cpyPower)
+            res[Len->lenOfCurrentNbr] = (((*cpyPower++ - '0') + (*tmp_curr++ - '0')) + flag);
+        else if(!(*tmp_curr) && *cpyPower)
+            res[Len->lenOfCurrentNbr] = *cpyPower++ - '0' + flag;
+        else
+            res[Len->lenOfCurrentNbr] = *tmp_curr++ -'0' + flag;
+        flag = 0;
+        if(res[Len->lenOfCurrentNbr] > 9)
+        {
+            flag = res[Len->lenOfCurrentNbr] / 10;
+            res[Len->lenOfCurrentNbr] %= 10;
+        }
+        Len->lenOfCurrentNbr++;
+    }
+    if (flag)
+    {
+        res[Len->lenOfCurrentNbr++] = -2;
+        res[Len->lenOfCurrentNbr++] = flag;
+    }
+    Len->lenOfResult = 0;
+    ft_strclr(curretNum);
     while(Len->lenOfCurrentNbr--)
     {
         curretNum[Len->lenOfResult] = (char) (res[Len->lenOfCurrentNbr] + '0');
@@ -128,18 +132,86 @@ void ft_plus_int(char *curretNum, char *cpyPower, t_len *Len)
     curretNum[Len->lenOfResult] = '\0';
 }
 
+char *ft_floatCpy(char *float_str, size_t len, t_len *Len)
+{
+    char *cpy_curr;
+    int i;
+    int j;
+
+    i = 0;
+    j = 0;
+    Len->lenOfIntPart = 0;
+    cpy_curr = (char *)malloc(sizeof(char) * (len + 1));
+    while(float_str[i] != '.' && float_str[i])
+    {
+        cpy_curr[i] = float_str[i];
+        Len->lenOfIntPart++;
+        i++;
+    }
+    j = i;
+    while (float_str[i++])
+    {
+        cpy_curr[j++] = float_str[i];
+        //i++;
+    }
+    return (cpy_curr);
+}
+
+char *ft_floatHandleRes(char *dest, char* src, size_t intLen)
+{
+    char *cpy_dest;
+
+    cpy_dest = dest;
+    while(intLen--)
+        *cpy_dest++ = *src++;
+    *cpy_dest++ = '.';
+    while(*src)
+        *cpy_dest++ = *src++;
+    return (dest);
+}
+
+void ft_makeEven(char *curretNum, char *powerTwo)
+{
+    size_t intCurrLen;
+    size_t intPowLen;
+    char *tmp;
+
+    intCurrLen = 0;
+    intPowLen = 0;
+    tmp = (char *)malloc(sizeof(char) * (ft_strlen(curretNum) + ft_strlen(powerTwo)));
+    while(curretNum[intCurrLen] != '.')
+        intCurrLen++;
+    while(powerTwo[intPowLen] != '.')
+        intPowLen++;
+    if(intCurrLen > intPowLen)
+    {
+        ft_memset(tmp, '0', intCurrLen - intPowLen);
+        ft_strcat(tmp, powerTwo);
+        ft_strcpy(powerTwo, tmp);
+        ft_strclr(tmp);
+    }
+    else
+    {
+        ft_memset(tmp, '0', intPowLen - intCurrLen);
+        ft_strcat(tmp, curretNum);
+        ft_strcpy(curretNum, tmp);
+        ft_strclr(tmp);
+    }
+}
+
 void ft_plus_float(char *curretNum, char *powerTwo, t_len *Len)
 {
     char *cpy_cur = NULL;
     char *cpy_power = NULL;
 
-    Len->lenOfCurrentNbr = ft_strlen(curretNum);
-    Len->lenOfPower = ft_strlen(powerTwo);
     if(!(*curretNum))
     {
         ft_strcpy(curretNum, powerTwo);
         return;
     }
+    ft_makeEven(curretNum, powerTwo);
+    Len->lenOfCurrentNbr = ft_strlen(curretNum);
+    Len->lenOfPower = ft_strlen(powerTwo);
 
     if(Len->lenOfCurrentNbr < Len->lenOfPower)
     {
@@ -151,13 +223,12 @@ void ft_plus_float(char *curretNum, char *powerTwo, t_len *Len)
         ft_memset((powerTwo) + (Len->lenOfPower),'0', (Len->lenOfCurrentNbr - Len->lenOfPower));
         Len->lenOfPower += Len->lenOfCurrentNbr - Len->lenOfPower;
     }
-    cpy_cur = ft_strsub(curretNum, 2, Len->lenOfCurrentNbr - 2);
-    cpy_power = ft_strsub(powerTwo, 2, Len->lenOfPower - 2);
+    cpy_cur = ft_floatCpy(curretNum, Len->lenOfCurrentNbr, Len);
+    cpy_power = ft_floatCpy(powerTwo, Len->lenOfPower, Len);
     ft_plus_int(cpy_cur, cpy_power, Len);
     if(ft_strlen(cpy_cur) > (Len->lenOfCurrentNbr > Len->lenOfPower ? Len->lenOfCurrentNbr - 2 : Len->lenOfPower - 2))
         curretNum[0] = *cpy_cur;
-    ft_strcpy(&(curretNum[2]), &(cpy_cur[0]));
-    Len->lenOfCurrentNbr = ft_strlen(cpy_cur) - 1;
+    curretNum = ft_floatHandleRes(curretNum, cpy_cur, Len->lenOfIntPart);
 }
 
 
@@ -226,6 +297,7 @@ char *ft_add_double(unsigned long mantissa, short exponent, size_t pricision)
     Len->lenOfResult = 0;
     Len->lenOfPower = 0;
     Len->lenOfCurrentNbr = 0;
+    Len->lenOfIntPart = 0;
     exponent++;
     while(cpyMantissa && exponent >= 0)
     {
--- main.c
+++ main.c
@@ -1,14 +1,4 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: ubartemi <ubartemi@student.42.fr>          +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2019/05/25 15:20:57 by ubartemi          #+#    #+#             */
-/*   Updated: 2019/07/01 19:04:03 by aestella         ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <limits.h>
@@ -17,56 +7,11 @@
 
 int main()
 {
-    int i = 42;
-    char *tmp;
-    char *s = (char*)malloc(sizeof(4));
-    tmp = s;
-    s = "abc";
-    //float g = 123.223;
-   // int *a = &i;
-    long long a = 1000000000000000000;
-    //printf("\n%d", ft_printf("test %s", "huest"));
-  /*  printf("\n%d\n", ft_printf("%.2x", i));
-    printf("\n%d\n", printf("%.2x", i));
-
-    printf("___________________\n");*/
-
-   // printf("___________________\n");
-
-   // ft_printf("%-+10.5d\n", 4242);
-   // printf("%-+10.5d\n", 4242);
-/*
-   printf("___________________\n");
-   ft_printf("%f|%f|%f\n", 1.e18, 1.e57, 1.e201);
-    printf("%f|%f|%f\n", 1.e18, 1.e57, 1.e201);
+    ft_printf("%+15.8f|\n", 0.);
+    printf("%+15.8f|\n", 0.);
     printf("___________________\n");
-*/
-char str = 5;
-    ft_printf("%f\n", 1.2);
-    printf("%f\n", 1.2);
-  /*  printf("~%d\n", ft_printf("%.f|%.f|%.f|%.f|%.f\n", 45.5, 45.4, 45.6, 45.50001, 45.5 + 1e-52));
-    printf("~%d\n", printf("%.f|%.f|%.f|%.f|%.f\n", 45.5, 45.4, 45.6, 45.50001, 45.5 + 1e-52));
-    printf("___________________\n");
+    ft_printf("% f|\n", -0.);
+    printf("% f|\n", -0.);
 
-*/
-    /*ft_printf("%5d\n", 42);
-    printf("%5d\n", 42);
-    printf("___________________\n");
-
-
-
-    ft_printf("%05d\n", -42);
-    printf("%05d\n", -42);
-    printf("___________________\n");
-
-    ft_printf("%-5d\n", -42);
-    printf("%-5d\n", -42);
-    printf("___________________\n");
-
-    ft_printf("%-05d\n", -42);
-    printf("%-05d\n", -42);
-    printf("___________________\n");*/
-
-    free(tmp);
     return  0;
 }
