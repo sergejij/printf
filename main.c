1d98c2a2e753f09507092dca9893ccb224501fbb
Prismari <m.ugorec@gmeil.com>
2019-07-03 15:51:10 +0300

    cleaned workspace

--- main.c
+++ main.c
@@ -4,14 +4,14 @@
 #include <limits.h>
 #include <float.h>
 #include "ft_printf.h"
+#include <math.h>
 
 int main()
 {
-    ft_printf("%+15.8f|\n", 0.);
-    printf("%+15.8f|\n", 0.);
-    printf("___________________\n");
-    ft_printf("% f|\n", -0.);
-    printf("% f|\n", -0.);
+
+    //ft_printf("%f\n", 1234.5678);
+    ft_printf("%f|\n", 17.9);
+   // printf("\n%f|%-f|%+f|% f|%#f|%0f\n", DBL_MAX, DBL_MAX, DBL_MAX, DBL_MAX, DBL_MAX, DBL_MAX);
 
     return  0;
 }
