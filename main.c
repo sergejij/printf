d6f7c81c80bd9fe2b474e4f5ccc4ee797658f7ed
Prismari <m.ugorec@gmeil.com>
2019-07-04 15:34:58 +0300

    uhh

--- main.c
+++ main.c
@@ -9,43 +9,37 @@
 int main()
 {
 
-    int a = 55;
+    int a = 123;
     int a_neg = -5;
-    //ft_printf("%f\n", 1234.5678);
+    //ft_printf("% 023i|%-+ 023i\n",  a, a);
+   // printf("% 023i|%-+ 023i\n", a, a);
 
     printf("My funk\n");
-    ft_printf("|%+023.5i|% 023.5i|%-+ 023.5i\n", INT_MAX, INT_MAX, INT_MAX);
+    ft_printf("%+21d\n", a, 0, 0);
     printf("origin:\n");
-    printf("|%+023.5i|% 023.5i|%-+ 023.5i\n", INT_MAX, INT_MAX, INT_MAX);
+    printf("%+21d\n", a, 0, 0);
 
-    printf("\n\n\n");
+
+    printf("3My funk\n");
+    ft_printf("%+21ld|%+23.ld|%+ 23ld|\n", LLONG_MAX, LLONG_MAX, LLONG_MAX);
+    printf("origin:\n");
+    printf("%+21ld|%+23.ld|%+ 23ld|\n", LLONG_MAX, LLONG_MAX, LLONG_MAX);
+
+printf("\n\n\n");
+    printf("4My funk\n");
+    ft_printf("%+23.ld|% 23.ld|%023.ld\n", LLONG_MAX, LLONG_MAX, LLONG_MAX);
+    printf("origin:\n");
+    printf("%+23.ld|% 23.ld|%023.ld\n", LLONG_MAX, LLONG_MAX, LLONG_MAX);
     printf("My funk\n");
-    ft_printf("%023.5i\n", INT_MAX);//%23.5i|%-23.5i|%+23.5i|% 23.5i|
+    ft_printf("%+23.5ld|% 23.5ld|%023.5ld\n", LLONG_MAX, LLONG_MAX, LLONG_MAX); //
     printf("origin:\n");
-    printf("%023.5i\n", INT_MAX);
-
-
-
-    printf("My funk\n");
-    ft_printf("%23d|%-23d|%+23d|% 23d|%023d\n", INT_MIN, INT_MIN, INT_MIN, INT_MIN, INT_MIN);
-    printf("origin:\n");
-    printf("%23d|%-23d|%+23d|% 23d|%023d\n", INT_MIN, INT_MIN, INT_MIN, INT_MIN, INT_MIN);
-
-
-    printf("My funk\n");
-    ft_printf("%23.5i|%-23.5i|%+23.5i|% 23.5i|%023.5i\n", INT_MIN, INT_MIN, INT_MIN, INT_MIN, INT_MIN);
-    printf("origin:\n");
-    printf("%23.5i|%-23.5i|%+23.5i|% 23.5i|%023.5i\n", INT_MIN, INT_MIN, INT_MIN, INT_MIN, INT_MIN);
-
+    printf("%+23.5ld|% 23.5ld|%023.5ld\n", LLONG_MAX, LLONG_MAX, LLONG_MAX);
 
     printf("My funk\n");
-    ft_printf("%-+5d|%- 5d|%-05d|%+ 5d|%+05d|% 05d|%-+ 05d|\n", 0, 0, 0, 0, 0, 0, 0);
+
+    ft_printf("%+ 23ld|%+023ld|% 023ld|%-+ 023ld\n",  LLONG_MAX, LLONG_MAX, LLONG_MAX, LLONG_MAX);
     printf("origin:\n");
-    printf("%-+5d|%- 5d|%-05d|%+ 5d|%+05d|% 05d|%-+ 05d|\n", 0, 0, 0, 0, 0, 0, 0);
-    //printf("%1.5d|%-1.5d|%+1.5d|% 1.5d|%01.5d\n", 0, 0, 0, 0, 0); //
-    //ft_printf("%1.5d|%-1.5d|%+1.5d|% 1.5d|%01.5d\n", 0, 0, 0, 0, 0);
-   // ft_printf("%+1.5d\n%+ 5.1d\n", 5, 5);
-   // printf("%+1.5d\n%+ 5.1d\n", 5, 5);
+    printf("%+ 23ld|%+023ld|% 023ld|%-+ 023ld\n",  LLONG_MAX, LLONG_MAX, LLONG_MAX, LLONG_MAX);
 
    // ft_printf("%1.5d|%-1.5d|%+1.5d|% 1.5d|%01.5d\n", a, a, a, a, a);
    // printf("%1.5d|%-1.5d|%+1.5d|% 1.5d|%01.5d\n", a, a, a, a, a);
